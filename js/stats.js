135650b4aa28e994fd3ea5021b5cceeba2c06281
webistomin <webistomin@gmail.com>
2018-01-06 16:05:40 +0300

    Начал добавление статистики

---
+++ js/stats.js
@@ -0,0 +1,9 @@
+//stats.js
+
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'white';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+}
