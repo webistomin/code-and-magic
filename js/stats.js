c33ef398fadfc5a45b587f7173a7e649198b5a1f
webistomin <webistomin@gmail.com>
2018-01-07 13:20:56 +0300

    Сделал цвета рандомными

--- js/stats.js
+++ js/stats.js
@@ -12,7 +12,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(100, 10, 420, 270);
 
   ctx.fillStyle = '#000000'; // текст внутри прямоугольника
-  ctx.font = '14px PT Mono';
+  ctx.font = '16px PT Mono';
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов: ', 120, 60);
 
@@ -32,17 +32,23 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillText('Худшее время: ' + max.toFixed() + 'мс у игрока ' + names[maxIndex], 120, 80);
 
-  var barWidth = 50; // px
-  var indent = 100; // px
+  var barWidth = 40; // px
+  var indent = 60; // px
   var initialX = 120; // px
   var initialY = 100; // px
   var lineHeight = 20; // px
-  var colors = ['rgba(215, 84, 20, 0.9)', 'rgba(124, 176, 20, 0.9)', 'rgba(124, 176, 157, 0.9)', 'rgba(124, 176, 255, 0.9)'];
 
   for (i = 0; i < times.length; i++) {
-    ctx.fillStyle = colors[i];
-    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step);
-    ctx.fillStyle = '#000000';
-    ctx.fillText(names[i], initialX + indent * i, initialY + histogramWidth + lineHeight);
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step);
+      ctx.fillStyle = '#000000';
+      ctx.fillText(names[i], initialX + indent * i, initialY + histogramWidth + lineHeight);
+    } else {
+      ctx.fillStyle = 'rgba(' + (Math.floor(Math.random() * 255)) + ', ' + (Math.floor(Math.random() * 255)) + ', ' + (Math.floor(Math.random() * 255)) + ', ' + (Math.random()) + ')';
+      ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step);
+      ctx.fillStyle = '#000000';
+      ctx.fillText(names[i], initialX + indent * i, initialY + histogramWidth + lineHeight);
+    }
   }
 };
