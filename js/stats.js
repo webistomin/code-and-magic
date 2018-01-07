6f43633ab555c70e6b9a2d7c4da199cbba629e1a
webistomin <webistomin@gmail.com>
2018-01-07 12:12:50 +0300

    Поправил тень

--- js/stats.js
+++ js/stats.js
@@ -3,8 +3,9 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270); // тень прямоугольника
-  ctx.fillStyle = 'rgba(0,0,0,0.7)';
+
 
   ctx.fillStyle = '#FFFFFF'; // прямоугльник с результатами
   ctx.strokeRect(100, 10, 420, 270);
