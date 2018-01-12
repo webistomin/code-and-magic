4a9c1b97daf8074c594ba29bf5fd0d7cce2a0b7b
webistomin <webistomin@gmail.com>
2018-01-12 15:49:59 +0300

    Add event listener

--- js/setup.js
+++ js/setup.js
@@ -6,42 +6,40 @@ var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
 var userNameInput = setup.querySelector('.setup-user-name');
+
 var onPopupEscPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     closePopup();
   }
 };
+
 var openPopup = function () {
   setup.classList.remove('hidden');
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ESC_KEYCODE) {
-      setup.classList.add('hidden');
-    }
-  });
+  document.removeEventListener('keydown', onPopupEscPress);
 };
+
 var closePopup = function () {
   setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
 };
 
-
-
 setupOpen.addEventListener('click', function () {
-  setup.classList.remove('hidden');
+  openPopup();
 });
 
 setupOpen.addEventListener('keydown', function (evt) {
   if (evt.keyCode === ENTER_KEYCODE) {
-    setup.classList.remove('hidden');
+    openPopup();
   }
 });
 
 setupClose.addEventListener('click', function () {
-  setup.classList.add('hidden');
+  closePopup();
 });
 
 setupClose.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 13) {
-    setup.classList.add('hidden');
+    closePopup();
   }
 });
 
