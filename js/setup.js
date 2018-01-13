cc957685735d137fca3260807ce3fd60b760a4c4
webistomin <webistomin@gmail.com>
2018-01-13 11:16:02 +0300

    add fireball color

--- js/setup.js
+++ js/setup.js
@@ -68,6 +68,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLOR =['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
@@ -140,3 +141,11 @@ var onClickChangeEyesColor = function () {
 };
 
 wizardEyes.addEventListener('click', onClickChangeEyesColor);
+
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+var onClickChangeFireballColor = function () {
+  wizardFireball.setAttribute('style', 'background-color: ' + FIREBALL_COLOR[Math.floor(Math.random() * FIREBALL_COLOR.length)]);
+};
+
+wizardFireball.addEventListener('click', onClickChangeFireballColor);
